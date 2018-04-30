e78f957a8222811fa5d2206fd65c97ef4c40b634
Zachary Mekus <zmekus@wustl.edu>
2018-04-29 20:15:33 -0500

    approval variances at each point

--- get_approval_data.py
+++ get_approval_data.py
@@ -2,6 +2,7 @@ import json
 from pprint import pprint
 import matplotlib.pyplot as plt
 import numpy as np
+import pickle
 
 plt.close("all")
 
@@ -23,18 +24,44 @@ for data in other_approval_json:
 	president_approval.append(approval)
 
 trump_approval = []
-date_today = '2018-03-29'
+approval_var = []
+approval_std = []
+date_today = '2018-04-02'
 for data in trump_approval_json:
 	if (data['date'] == date_today): break
 	if (data['subgroup'] == 'All polls'): # 'Adults' 'Voters'
-		trump_approval.append(data['approve_estimate'])
+		approval = float(data['approve_estimate'])
+		trump_approval.append(approval)
+		high = float(data['approve_hi'])
+		low = float(data['approve_lo'])
+		dev = high-approval
+		num_stds = 1.64 # 90% confidence interval
+		std = dev / num_stds
+		approval_std.append(std)
+		var = std * std
+		approval_var.append(var)
+print(approval_var)
+print(np.mean(approval_var))
+print(np.mean(approval_std) ** 2)
+
+pickle.dump(approval_var, open("approval_vars", "wb"))
+
 presidents['Donald Trump'] = trump_approval
 
+trump_array = np.array(trump_approval)
+pickle.dump(trump_array, open("approval","wb"))
 
-for president, approval in presidents.items():
-	plt.figure()
-	plt.plot(approval)
-	plt.title(president)
+rep_approval = pickle.load(open("rep_estimates","rb"))
 
+plt.plot(trump_approval[82:])
+plt.plot(rep_approval[:-15])
 plt.show()
+
+
+# for president, approval in presidents.items():
+# 	plt.figure()
+# 	plt.plot(approval)
+# 	plt.title(president)
+
+#plt.show()
 
